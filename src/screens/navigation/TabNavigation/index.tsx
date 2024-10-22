88c4b0a46bca79d52e1baabb93a08e2b4a9b7b20
kbett035 <kimutaiwyclife6@gmail.com>
2024-10-23 00:47:56 +0300

    Authentication Complete

--- src/screens/navigation/TabNavigation/index.tsx
+++ src/screens/navigation/TabNavigation/index.tsx
@@ -4,9 +4,10 @@ import { View, Text } from 'react-native';
 const TabNavigation = () => {
   return (
     <View className="flex-1 bg-blue-400 justify-center items-center">
-      <Text>Index</Text>
+      <Text>Welcome to Tab Navigation</Text>
     </View>
   );
 };
 
 export default TabNavigation;
+ 
