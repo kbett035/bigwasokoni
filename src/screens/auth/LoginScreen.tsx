09e01be839477fd99f1dca33d05be82691d52b89
kbett035 <kimutaiwyclife6@gmail.com>
2024-10-22 16:50:57 +0300

    Start of Login screen

--- src/screens/auth/LoginScreen.tsx
+++ src/screens/auth/LoginScreen.tsx
@@ -1,12 +1,56 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, ActivityIndicator, Dimensions } from 'react-native';
+import Animated, { FadeInDown } from 'react-native-reanimated';
+
+const { width, height } = Dimensions.get('window');
 
 const LoginScreen = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+
   return (
-    <View>
+    <View className="flex-1">
+      {isLoading && (
+        <View className="absolute z-50 h-full w-full justify-center items-center">
+          <View
+            className="h-full w-full justify-center items-center bg-black"
+            style={{ opacity: 0.45 }}
+          />
+          <View className="absolute">
+            <ActivityIndicator size="large" color="white" />
+          </View>
+        </View>
+      )}
+
+      <View className="justify-center items-center relative flex-1">
+        <View
+          className="justify-center w-full px-4 space-y-4"
+          style={{
+            height: height * 0.75,
+          }}
+        >
+          {/* Welcome Text */}
+          <Animated.View
+            className="justify-center items-center"
+            entering={FadeInDown.duration(100).springify()}
+          >
+            <Text
+              className="text-neutral-800 text-2xl"
+              style={{
+                fontFamily: 'PlusJakartaSansBold',
+                lineHeight: 60, // replaces leading in Tailwind-like syntax
+              }}
+            >
+              Welcome Back, User
+            </Text>
+          </Animated.View>
+        </View>
+      </View>
+
       <Text>LoginScreen</Text>
     </View>
   );
-}
+};
 
 export default LoginScreen;
