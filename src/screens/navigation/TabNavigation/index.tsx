f4c0a14cbd2fec8b306f15745176d1b33c070e9a
kbett035 <kimutaiwyclife6@gmail.com>
2024-10-23 08:59:30 +0300

    Added bottom tabs

--- src/screens/navigation/TabNavigation/index.tsx
+++ src/screens/navigation/TabNavigation/index.tsx
@@ -1,13 +1,51 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from "@expo/vector-icons";
+import HomeNavigation from './HomeNavigation';
+import OffersNavigation from './OffersNavigation';
+import HistoryNavigation from './HistoryNavigation';
+import AlertsNavigation from './AlertsNavigation';
+import ProfileNavigation from './ProfileNavigation';
+
+const Tab = createBottomTabNavigator();
 
 const TabNavigation = () => {
   return (
-    <View className="flex-1 bg-blue-400 justify-center items-center">
-      <Text>Welcome to Tab Navigation</Text>
-    </View>
+    <Tab.Navigator
+      screenOptions={
+        ({ route }) => {
+          const icons: Record<string, keyof typeof Ionicons.glyphMap> = {
+            Home: "home-outline",
+            Offers: "pricetags-outline",
+            Alerts: "notifications-outline",
+            History: "time-outline",
+            Profile: "person-outline", // Ensured it's from the Ionicons set
+          };
+
+          return {
+            headerShown: false,
+            tabBarIcon: ({ focused }) => {
+              const iconName = icons[route.name]; // Ensuring that the name is from the predefined set
+              const customizeSize = 25;
+              return (
+                <Ionicons
+                  name={iconName}
+                  size={customizeSize}
+                  color={focused ? "#164b48" : "gray"}
+                />
+              );
+            },
+          };
+        }
+      }
+    >
+      <Tab.Screen name="Home" component={HomeNavigation} />
+      <Tab.Screen name="Offers" component={OffersNavigation} />
+      <Tab.Screen name="History" component={HistoryNavigation} />
+      <Tab.Screen name="Alerts" component={AlertsNavigation} />
+      <Tab.Screen name="Profile" component={ProfileNavigation} />
+    </Tab.Navigator>
   );
 };
 
 export default TabNavigation;
- 
