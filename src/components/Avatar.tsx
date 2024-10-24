bf7ae0127aac4a3e57885a3392567a3e9b52ff0c
kbett035 <kimutaiwyclife6@gmail.com>
2024-10-24 16:43:26 +0300

    Top up image updated and fixed

--- src/components/Avatar.tsx
+++ src/components/Avatar.tsx
@@ -1,12 +1,99 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, View, ActivityIndicator, Image, Pressable } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
+import { supabase } from '@/lib/supabase';
 
-const Avatar = () => {
-  return (
-    <View>
-      <Text>Avatar</Text>
-    </View>
-  );
+interface Props {
+  size: number;
+  url: string | null;
+  onUpload: (filePath: string) => void;
+  showUpload?: boolean;
 }
 
+const Avatar = ({ url, size = 150, onUpload, showUpload }: Props) => {
+  const [uploading, setUploading] = useState(false);
+  const [avatarUrl, setAvatarUrl] = useState<string | null>(null);
+  const avatarSize = { height: size, width: size };
+
+  useEffect(() => {
+    if (url) downloadImage(url);
+  }, [url]);
+
+  async function downloadImage(path: string) {
+    try {
+      const { data, error } = await supabase.storage.from('avatars').download(path);
+
+      if (error) {
+        console.log(error); // Log the error before throwing it
+        throw error;
+      }
+
+      // Create a blob URL from the data
+      const avatarBlobUrl = URL.createObjectURL(data);
+      setAvatarUrl(avatarBlobUrl);
+    } catch (error) {
+      if (error instanceof Error) {
+        console.log('Error downloading image: ', error.message);
+      }
+    }
+  }
+
+  async function uploadAvatar() {
+    // Implement the upload logic here
+  }
+
+  return (
+    <View>
+      {avatarUrl ? (
+        <View style={{ position: 'relative' }}>
+          <Image
+            source={{ uri: avatarUrl }}
+            accessibilityLabel="Avatar"
+            style={[avatarSize, styles.avatar, styles.image]}
+          />
+        </View>
+      ) : (
+        <View
+          style={[avatarSize, styles.avatar, styles.noImage]}
+          className="justify-center items-center"
+        >
+          <ActivityIndicator color="white" />
+          {/* Optional: Add fallback text */}
+          {/* <Text>No Avatar</Text> */}
+        </View>
+      )}
+      {showUpload && (
+        <View style={{ position: 'absolute', right: 0, bottom: 0 }}>
+          {!uploading ? (
+            <Pressable onPress={uploadAvatar}>
+              <MaterialIcons name="cloud-upload" size={30} color="black" />
+            </Pressable>
+          ) : (
+            <ActivityIndicator color="white" />
+          )}
+        </View>
+      )}
+    </View>
+  );
+};
+
 export default Avatar;
+
+const styles = StyleSheet.create({
+  avatar: {
+    overflow: 'hidden',
+    maxWidth: '100%',
+    position: 'relative',
+  },
+  image: {
+    objectFit: 'cover',
+    paddingTop: 0,
+  },
+  noImage: {
+    backgroundColor: 'gray',
+    borderWidth: 2,
+    borderStyle: 'solid',
+    borderColor: 'rgb(200, 200, 200)',
+    borderRadius: 20,
+  },
+});
--- src/screens/tabs/home/HomeScreen.tsx
+++ src/screens/tabs/home/HomeScreen.tsx
@@ -1,30 +1,116 @@
-import React from 'react';
+import useSupabaseAuth from '@/hooks/useSupabaseAuth';
+import Avatar from '@/src/components/Avatar';
+import { useUserStore } from '@/store/useUserStore';
+import { Ionicons } from '@expo/vector-icons';
+import { useFocusEffect } from '@react-navigation/native';
+import React, { useState, useEffect, useCallback } from 'react';
 import { View, Text } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { Image } from "expo-image";
+
 
 const HomeScreen = () => {
+  const [avatarUrl, setAvatarUrl] = useState("");
+  const [username, setUsername] = useState("");
+  const [loading, setLoading] = useState(false);
+  const { getUserProfile } = useSupabaseAuth();
+  const { session } = useUserStore();
+
+  const blurhash = "|rF?hV%2WCj[ayj[a|j[az_NaeWBj@ayfRayfQfQM{M|azj[azf6fQfQfQIpWXofj[ayj[j[fQayWCoeoeaya}j[ayfQa{oLj?j[WVj[ayayj[fQoff7azayj[ayj[j[ayofayayayj[fQj[ayayj[ayfjj[j[ayjuayj[";
+
+  // Function to fetch user profile
+  async function handleGetProfile() {
+    setLoading(true);
+    try {
+      const { data, error, status } = await getUserProfile();
+
+      if (error && status !== 406) {
+        throw error;
+      }
+
+      if (data) {
+        setUsername(data.username);
+        setAvatarUrl(data.avatar_url);
+      }
+    } catch (error) {
+      console.error("Error fetching profile:", error);
+    } finally {
+      setLoading(false); // Ensures loading is set to false in both cases
+    }
+  }
+
+  useFocusEffect(
+    useCallback(() => {
+      if (session) {
+        handleGetProfile();
+      }
+    }, [session])
+  );
+
   return (
     <SafeAreaView className="flex-1 bg-white">
       <View className="relative">
 
         {/* Header */}
-        <View className="w-full flex-row justufy-between items-center px-4">
+        <View className="w-full flex-row justify-between items-center px-4">
 
           <View className="w-3/4 flex-row space-x-2">
+            <View className="justify-center items-center">
+              <View className="h-12 w-12 rounded-2xl overflow-hidden">
+                <Avatar
+                  url={avatarUrl}
+                  size={50}
+                  onUpload={function (filePath: string): void {
+                    throw new Error('Function not implemented.');
+                  }}
+                />
+              </View>
+            </View>
 
-        <View className="justify-center items-center">
+            <View>
+              <Text className="text-lg font-bold">Hi, {username ? username : "User"}</Text>
+              <Text className="text-sm text-neutral-500">Have a good day</Text>
+            </View>
+          </View>
 
-          <View className="h-12 w-12 rounded-2xl overflow">
-
+          {/* Menu Icon */}
+          <View className="py-6">
+            <View className="bg-neutral-700 rounded-lg p-1">
+              <Ionicons name="menu" size={24} color="white" />
+            </View>
           </View>
 
         </View>
-         
-          </View>
-        </View>
+        {/* Balance */}
 
+        <View className="mx-4 bg-neutral-800 rounded-[34px] overflow-hidden mt-4 mb-4">
+          <View className="bg-[#0DF69E] justify-center items-center py-6 rounded-[34px] ">
+            <Text className="text-sm font-medium text-neutral-700">
+              Airtime Balance
+            </Text>
+
+            <Text className="text-3xl font-extrabold">
+             2,345.00
+            </Text>
+
+          </View>
+          <View className="justify-between items-center flex-row py-4">
+            <View className="w-1/4 justify-center items-center space-y-2">
+              <View className="w-10 h-10 overflow-hidden bg-[#3B363F] rounded-full p-2">
+                <Image
+                  source={require("../../../../assets/images/money-send.png")}
+                  placeholder={blurhash}
+                  contentFit="cover"
+                  transition={1000}
+                  className="w-full h-full flex-1"
+                />
+              </View>
+              <Text className="text-white">Top Up</Text>
+            </View>
+          </View>
+
+        </View>
       </View>
-
     </SafeAreaView>
   );
 }
